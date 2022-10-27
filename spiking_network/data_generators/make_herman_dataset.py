2474482ba32dd4f3d746e9bbd68a5ee0795e488f
Herman Brunborg <herman@brunborg.com>
2022-10-27 13:45:18 +0200

    Adding emptiness to argparser and Herman dataset

--- spiking_network/data_generators/make_herman_dataset.py
+++ spiking_network/data_generators/make_herman_dataset.py
@@ -34,35 +34,37 @@ def herman_save(x, model, w0_data, seed, data_path, n_neurons, stimulation=None)
 def calculate_isi(spikes: np.ndarray, N, n_steps, dt=0.0001) -> float:
     return N * n_steps * dt / spikes.sum()
 
-def make_herman_dataset(n_neurons, n_sims, n_steps, data_path, max_parallel, firing_rate=0.016):
+def make_herman_dataset(n_neurons, n_sims, n_steps, data_path, max_parallel, firing_rate=0.016, emptiness=0.9):
     # Path to save results
-    data_path = "spiking_network"/ Path(data_path) / Path(f"herman_{n_neurons}_neurons_{n_sims}_sims_{n_steps}_steps")
+    data_path = Path(data_path) / Path(f"herman_{n_neurons}_neurons_{n_sims}_sims_{n_steps}_steps")
     data_path.mkdir(parents=True, exist_ok=True)
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Reproducibility
     rng = torch.Generator().manual_seed(0)
     seeds = {
-            "w0": torch.randint(0, 100000, (n_sims,), generator=rng).tolist(),
-            "model": torch.randint(0, 100000, (1,), generator=rng).item(),
-         }
+        "w0": torch.randint(0, 100000, (n_sims,), generator=rng).tolist(),
+        "model": torch.randint(0, 100000, (1,), generator=rng).item(),
+     }
 
-    model_path = Path("spiking_network/models/saved_models") / f"herman_{n_neurons}_neurons_{firing_rate}_firing_rate.pt"
-    model = HermanModel(
-            seed=seeds["model"],
-            device=device
-        ).load(model_path) if model_path.exists() else HermanModel(seed=seeds["model"], device=device)
+    #  model_path = Path("models/saved_models") / f"herman_{n_neurons}_neurons_{firing_rate}_firing_rate.pt"
+    #  model = HermanModel(
+    #      seed=seeds["model"],
+    #      device=device
+    #  ).load(model_path) if model_path.exists() else HermanModel(seed=seeds["model"], device=device)
+    model = HermanModel(seed=seeds["model"], device=device)
 
     batch_size = min(n_sims, max_parallel)
-    herman_dataset = HermanDataset(n_neurons, n_sims, seed=seeds["w0"])
+    herman_dataset = HermanDataset(n_neurons, n_sims, seed=seeds["w0"], emptiness=emptiness)
     data_loader = DataLoader(herman_dataset, batch_size=batch_size, shuffle=False)
     results = []
     for batch_idx, data in enumerate(data_loader):
         data = data.to(device)
-        
-        stim = RegularStimulation(targets=0, strengths=1, duration=n_steps, rates=0.2, temporal_scales=2, n_neurons=data.num_nodes, device=device)
-        spikes = model.simulate(data, n_steps, stimulation=stim)
-        print(f"ISI: {calculate_isi(spikes, n_neurons, n_steps)}")
+
+        #  stim = RegularStimulation(targets=0, strengths=1, duration=n_steps, rates=0.2, temporal_scales=2, n_neurons=data.num_nodes, device=device)
+        #  spikes = model.simulate(data, n_steps, stimulation=stim)
+        spikes = model.simulate(data, n_steps)
+        #  print(f"ISI: {calculate_isi(spikes, n_neurons, n_steps)}")
         results.append(spikes)
 
     save(results, model, herman_dataset, seeds, data_path)
